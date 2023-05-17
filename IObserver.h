319ed99299ae03b99bc5d7c8e7f68bb26f4ef781
PodanevaAN <101283624+PodanevaAN@users.noreply.github.com>
2023-05-17 09:50:02 +0300

    Update IObserver.h
    добавила слот

--- IObserver.h
+++ IObserver.h
@@ -2,7 +2,7 @@
 #define IOBSERVER_H
 #include <QObject>
 
-class IObserver : public QObject  {
+class IObserver : public QObject  { // Определение интерфейса IObserver, который наследует от класса QObject
     Q_OBJECT
 public slots:
     virtual void update(long size) = 0; //Виртуальный общий метод для всех наблюдателей
