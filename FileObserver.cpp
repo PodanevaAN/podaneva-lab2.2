028799638a1615f4889f5d2ee78c99f1c92660df
PodanevaAN <101283624+PodanevaAN@users.noreply.github.com>
2023-05-18 20:43:29 +0300

    Update FileObserver.cpp
    добавила updateExist

--- FileObserver.cpp
+++ FileObserver.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "FileObserver.h"
 
-void FileObserver::update(long size) { //Метод выводит информацию если состояние файла изменилось
+void FileObserver::updateSize(long size) { //Метод выводит информацию если состояние файла изменилось
     if (_size != size) { //Если предыдущий размер файл не совпадает с новым, значит файл изменился
-        if (size == -1) { //Если размер -1 значит файл не существует
-            std::cout << "File doesn't exist" << std::endl;
-        } else { //Иначе выводим размер файла
-            std::cout << "File exists. Size = " << size << std::endl;
+            if (size == -1) { //Если размер -1 значит файл не существует
+                std::cout << "File doesn't exist" << std::endl;
+            } else { //Иначе выводим размер файла
+                std::cout << "File exists. Size = " << size << std::endl;
+            }
+            _size = size; //Сохраняем новый размер
         }
-        _size = size; //Сохраняем новый размер
-    }
+}
+void FileObserver::updateExist(bool exist) {
+
+        _exist = exist;
 }
