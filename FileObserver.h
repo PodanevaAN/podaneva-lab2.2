fc1ae36c1ff99a7d25c7c69451a9960c6bbdcb86
PodanevaAN <101283624+PodanevaAN@users.noreply.github.com>
2023-05-18 20:44:23 +0300

    Update FileObserver.h
    добавила слот

--- FileObserver.h
+++ FileObserver.h
@@ -1,15 +1,13 @@
 #ifndef FILEOBSERVER_H
 #define FILEOBSERVER_H
 
-#include "IObserver.h"
-
 class FileObserver : public IObserver {
 private:
     long _size = -1; //размер файла. -1 если файл не существует
-
+    bool _exist = 0;//файл не существует
 public slots:
-    void update(long size) override; //// Переопределение метода update() из интерфейса IObserver
+    void updateSize(long size) ;//переопределены
+    void updateExist(bool exist);//Переопределяем метод
 };
-
 
 #endif //FILEOBSERVER_H
