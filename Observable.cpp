8326eced5e4710cd44217515bdf1de371bb05788
PodanevaAN <101283624+PodanevaAN@users.noreply.github.com>
2023-05-18 20:49:02 +0300

    Update Observable.cpp
    Добавила connect and emit

--- Observable.cpp
+++ Observable.cpp
@@ -3,18 +3,23 @@
 
 void Observable::attach(IObserver *observer) {
     observers.push_back(observer); //Добавляет нового наблюдателя
-     QObject::connect(this,&Observable::notifySize,observer,&IObserver::update); //Устанавливает связь между сигналом и слотом update у каждого наблюдателя
+     QObject::connect(this,&Observable::signalSize,observer,&IObserver::updateSize); //Устанавливает связь между сигналом и слотом updateSize у каждого наблюдателя
+     QObject::connect(this,&Observable::signalExist,observer,&IObserver::updateExist);//Устанавливает связь между сигналом и слотом updateExist у каждого наблюдателя
+
 }
 
 void Observable::detach(IObserver *observer) {
     observers.erase(std::remove(observers.begin(), observers.end(), observer), observers.end()); //Удаляет наблюдателя
 }
 
-void Observable::notify(long size) { //Оповещает всех наблюдателей
+void Observable::send(long size, bool exist) { //Оповещает всех наблюдателей
 //    for (const auto &observer: observers) { //Провходит по всем наблюдателям из вектора
 //        if (observer != nullptr) { //Если наблюдатель не нулевой
 //            observer->update(size); //Вызываем функцию для обновления информации
 //        }
 //    }
-    emit notifySize(size); //Испускает сигнал notifySize с переданным размером, который будет обработан наблюдателями
+    emit signalSize(size); //Испускает сигнал signalSize с переданным размером, который будет обработан наблюдателями
+    emit signalExist(exist); //Испускает сигнал signalExist с переданным наличием, который будет обработан наблюдателями
 }
+
+  
